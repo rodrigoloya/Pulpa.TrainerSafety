5d1773fb48df96c028f52324e6107eba459d916d
RloyaDesktop <al_corp@hotmail.com>
2025-11-30 23:12:09 -0500

    Add static Roles class for role-based access control
    Introduce a new static `Roles` class in the `Pulpa.TrainerSafety.Domain` namespace. The class defines constants for role names (`Admin`, `Member`, and `User`) to standardize role definitions across the application. Added necessary `using` directives for required namespaces.

---
+++ Pulpa.TrainerSafety.Domain/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pulpa.TrainerSafety.Domain
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Member = "Member";
+        public const string User = "User";
+    }
+}
