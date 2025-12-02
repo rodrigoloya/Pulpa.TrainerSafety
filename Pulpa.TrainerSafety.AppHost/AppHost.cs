e6e51dc15854266b2d3c9d3a5bfb5c9dcac21608
RloyaDesktop <al_corp@hotmail.com>
2025-12-01 19:34:05 -0500

    Add configuration support for database and JWT secrets
    Updated `AppHost.cs` to use `Microsoft.Extensions.Configuration` for accessing configuration values. Added environment variable setup for `ConnectionStrings__DefaultConnection` and `Jwt__SecretKey` in the `pulpa-trainersafety-api` project. Introduced `WithExternalHttpEndpoints` for external HTTP endpoint configuration.
    
    Modified `appsettings.json` to include a `ConnectionStrings` section with a `DefaultConnection` key for database connection and a `Jwt:SecretKey` key for JWT authentication.

--- Pulpa.TrainerSafety.AppHost/AppHost.cs
+++ Pulpa.TrainerSafety.AppHost/AppHost.cs
@@ -1,8 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-
-
 builder.AddProject<Projects.Pulpa_TrainerSafety_Api>("pulpa-trainersafety-api")
+    .WithEnvironment("ConnectionStrings__DefaultConnection",
+            builder.Configuration.GetConnectionString("DefaultConnection"))
+    .WithEnvironment("Jwt__SecretKey", builder.Configuration.GetValue<string>("Jwt:SecretKey"))
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
